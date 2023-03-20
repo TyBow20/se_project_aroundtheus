4bf8d024afd5c75dffa605d99e10a979840cd275
Tyler Bowman <tbowman20@gmail.com>
2023-03-20 12:11:15 -0400

    4.FS

--- scripts/index.js
+++ scripts/index.js
@@ -27,16 +27,28 @@ const initialCards = [
 
   const profileEditButton = document.querySelector("#profile-edit-button");
   const profileEditModal = document.querySelector('#profile-edit-modal');
+  const profileCloseButton = document.querySelector("#profile-close-button");
+  const profileTitle = document.querySelector(".profile__title");
+  const profileDescription = document.querySelector(".profile__description");
+  const profileInput = document.querySelector("#profile-title-input");
+  const profileDescriptionInput = document.querySelector("#profile-description-input");
 
+// console.log(profileDescription.textContent);
+
+  const profileTitleInput = document.querySelector('.modal-name-item');
 
   profileEditButton.addEventListener("click", () => {
+  profileInput.value =profileTitle.textContent;
+  profileDescriptionInput.value =profileDescription.textContent;
   profileEditModal.classList.add('modal_opened');
 });
-
-const profileCloseButton = document.querySelector("#profile-close-button");
-// const profileCloseModal = document.querySelector('#profile-edit-modal');
 
 profileCloseButton.addEventListener("click", () => {
   profileEditModal.classList.remove('modal_opened');
 });
-  
+
+profileEditButton.addEventListener('submit', (e) => {
+  e.preventDefault();
+  profileTitle.textContent =profileInput.value;
+  profileDescription.textContent =profileDescriptionInput.value;
+});
