fdbee372a291e21c34e1ee52fffd297525094398
Tyler Bowman <tbowman20@gmail.com>
2023-03-23 18:16:34 -0400

    4.final

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,42 @@
 const initialCards = [
-    {
-      name: "Yosemite Valley",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
-    },
-    {
-      name: "Lake Louise",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
-    },
-    {
-      name: "Bald Mountians",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
-    },
-    {
-      name: "Latemar",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
-    },
-    {
-      name: "Vanoise National Park",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
-    },
-    {
-      name: "Lago di Braies",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
-    }
-  ];
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountians",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
 
-  const profileEditButton = document.querySelector("#profile-edit-button");
-  const profileEditModal = document.querySelector('#profile-edit-modal');
-  const profileCloseButton = document.querySelector("#profile-close-button");
-  const profileTitle = document.querySelector(".profile__title");
-  const profileDescription = document.querySelector(".profile__description");
-  const profileInput = document.querySelector("#profile-title-input");
-  const profileDescriptionInput = document.querySelector("#profile-description-input");
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileCloseButton = document.querySelector("#profile-close-button");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileNameInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
 
-  const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
-
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 
 // function getCardElement(cardData) {
 //   const cardElement = cardTemplate.cloneNode(true);
@@ -45,43 +47,41 @@ const initialCards = [
 
 function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
-  const cardImageEl = cardElement.querySelector('.card__image');
-  const cardTitleEl = cardElement.querySelector('.card__title');
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
   cardTitleEl.textContent = cardData.name;
   cardImageEl.src = cardData.link;
   cardImageEl.alt = cardData.name;
   return cardElement;
 }
 
+profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".gallery__cards");
 
-
-  profileEditForm = profileEditModal.querySelector(".modal__form");
-  const cardListEl = document.querySelector('.gallery__cards');
-
-  profileEditButton.addEventListener("click", () => {
-  profileInput.value =profileTitle.textContent;
-  profileDescriptionInput.value =profileDescription.textContent;
-  profileEditModal.classList.add('modal_opened');
+profileEditButton.addEventListener("click", () => {
+  profileNameInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+  profileEditModal.classList.add("modal_opened");
 });
 
 profileCloseButton.addEventListener("click", () => {
-  profileEditModal.classList.remove('modal_opened');
+  profileEditModal.classList.remove("modal_opened");
 });
 
-profileEditForm.addEventListener('submit', (e) => {
+profileEditForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  profileTitle.textContent =profileInput.value;
-  profileDescription.textContent =profileDescriptionInput.value;
-  profileEditModal.classList.remove('modal_opened');
+  profileTitle.textContent = profileNameInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  profileEditModal.classList.remove("modal_opened");
 });
 
 initialCards.forEach((cardData) => {
-// const cardElement = cardTemplate.cloneNode(true);
-// const cardImageEl = cardElement.querySelector('.card__image');
-// const cardTitleEl = cardElement.querySelector('.card__title');
-// cardTitleEl.textContent = cardData.name;
-// return cardElement;
-const cardElement = getCardElement(cardData);
-// const cardImageEl = cardElement.querySelector('.gallery__cards');
-cardListEl.append(cardElement);
+  // const cardElement = cardTemplate.cloneNode(true);
+  // const cardImageEl = cardElement.querySelector('.card__image');
+  // const cardTitleEl = cardElement.querySelector('.card__title');
+  // cardTitleEl.textContent = cardData.name;
+  // return cardElement;
+  const cardElement = getCardElement(cardData);
+  // const cardImageEl = cardElement.querySelector('.gallery__cards');
+  cardListEl.append(cardElement);
 });
